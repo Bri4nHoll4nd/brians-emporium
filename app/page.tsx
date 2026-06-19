da64c5b5c0a6bb37786d9215b0961e8b9cf08d2a
Bakk-e <jorgen-bakke@hotmail.com>
2026-06-19 21:55:26 +0200

    made some changes to navbar

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     const router = useRouter();
 
     return(
-        <div id="main-page-body">
+        <div className="body">
             <Navbar isLoggedIn={isLoggedIn}></Navbar>
             <main>
                 <div id="noteworthy-sales">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,5 @@
 import SearchBar from "@/components/SearchBar";
 
-//className={`navbar-top-searchbar-clear ${searchTerm == "" ? 'hidden' : ''}`} onClick={clearSearch}
-
 type NavbarProps = {
     isLoggedIn: boolean
 }
@@ -14,30 +12,29 @@ export default function Navbar({ isLoggedIn }: NavbarProps) {
                 <SearchBar></SearchBar>
                 {isLoggedIn ? (
                     <a id="navbar-top-profile">
-                        <img src="/images/Profile.svg"
+                        <img src="/images/logos/Profile.svg"
                         alt="Profile Logo"/>
                         <span>Profile</span>
                     </a>
                 ) : (
                     <a id="navbar-top-login">
-                        <img src="/images/Login.svg"
+                        <img src="/images/logos/Login.svg"
                         alt="Profile logo"/>
                         <span>Login</span>
                     </a>
                 )}
                 <a id="navbar-top-cart">
-                    <img src="/images/Cart.svg"
+                    <img src="/images/logos/Cart.svg"
                     alt="Cart logo"/>
                     <span>Cart</span>
                 </a>
             </div>
             <div id="navbar-bottom">
                 <button>Computing</button>
-                <button>Phone</button>
+                <button>Phones & Tablets</button>
                 <button>Tv & Sound</button>
                 <button>Gaming</button>
                 <button>Home & Leisure</button>
-                <button>Toys & Hobby</button>
                 <button>Clothing</button>
             </div>
         </header>
